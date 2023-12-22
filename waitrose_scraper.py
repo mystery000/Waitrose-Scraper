ec24e3597fbceac60b1c2409c90f56757b42e36c
MYSTERY000 <lkostyrka17@gmail.com>
2023-12-21 21:35:36 -0500

    Fix commit

--- waitrose_scraper.py
+++ waitrose_scraper.py
@@ -36,8 +36,6 @@ class ProductScraper:
                 with Remote(self._sbr_connection, options=chrome_options) as driver:
                     product_url = self.get_product_page_link(product["name"], product["id"])
                     
-                    driver.implicitly_wait(0.2)
-                    
                     driver.get(product_url)
                     
                     html = driver.page_source
