56822bc5f2a047bad45cff34eefa8df59fa0736a
MYSTERY000 <lkostyrka17@gmail.com>
2023-12-27 14:33:24 -0500

    Update Selenium Server IP addresses

--- waitrose_scraper.py
+++ waitrose_scraper.py
@@ -197,9 +197,9 @@ def run_waitrose_scraper():
     
     try:
         SELENIUM_GRID_IP_ADDRESSES = [
-            "95.217.141.220:9515",
-            "65.21.129.16:9515",
-            "135.181.212.76:9515",
+            "18.169.27.82:9515",
+            "13.42.66.41:9515",
+            "18.171.169.136:9515"
         ]
         
         sbr_connections = [ChromiumRemoteConnection(f"http://{IP}", "goog", "chrome") for IP in SELENIUM_GRID_IP_ADDRESSES]
